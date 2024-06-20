978445e3c0a84506c10643a1d5cd5723a6503aee
Moacir Braga <contato@moacirbrg.org>
2024-06-19 21:44:55 -0300

    WGPU with GLFW Window

---
+++ wgpu-with-window/src/main.rs
@@ -0,0 +1,180 @@
+use glfw::{fail_on_errors, Context};
+
+struct State<'a> {
+    instance: wgpu::Instance,
+    surface: wgpu::Surface<'a>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: (i32, i32),
+    window: &'a mut glfw::Window,
+}
+
+impl<'a> State<'a> {
+    async fn new(window: &'a mut glfw::Window) -> Self {
+        let size = window.get_framebuffer_size();
+
+        let instance_descriptor = wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            ..Default::default()
+        };
+        let instance = wgpu::Instance::new(instance_descriptor);
+
+        let target = unsafe { wgpu::SurfaceTargetUnsafe::from_window(&window) }.unwrap();
+        let surface = unsafe { instance.create_surface_unsafe(target) }.unwrap();
+
+        let adapter_descriptor = wgpu::RequestAdapterOptionsBase {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        };
+        let adapter = instance.request_adapter(&adapter_descriptor).await.unwrap();
+
+        let device_descriptor = wgpu::DeviceDescriptor {
+            required_features: wgpu::Features::empty(),
+            required_limits: wgpu::Limits::default(),
+            label: Some("Device"),
+        };
+        let (device, queue) = adapter.request_device(&device_descriptor, None).await.unwrap();
+
+        let surface_capabilities = surface.get_capabilities(&adapter);
+        let surface_format = surface_capabilities.formats.iter()
+            .copied().filter(|f| f.is_srgb())
+            .next().unwrap_or(surface_capabilities.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.0 as u32,
+            height: size.1 as u32,
+            present_mode: surface_capabilities.present_modes[0],
+            alpha_mode: surface_capabilities.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2
+        };
+
+        surface.configure(&device, &config);
+
+        Self {
+            instance,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            window,
+        }
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let drawable = self.surface.get_current_texture()?;
+        let image_view_descriptor = wgpu::TextureViewDescriptor::default();
+        let image_view = drawable.texture.create_view(&image_view_descriptor);
+
+        let command_encoder_descriptor = wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        };
+        let mut command_encoder = self.device.create_command_encoder(&command_encoder_descriptor);
+
+        let color_attachment = wgpu::RenderPassColorAttachment {
+            view: &image_view,
+            resolve_target: None,
+            ops: wgpu::Operations {
+                load: wgpu::LoadOp::Clear(wgpu::Color {
+                    r: 0.25,
+                    g: 0.0,
+                    b: 0.0,
+                    a: 0.0,
+                }),
+                store: wgpu::StoreOp::Store,
+            },
+        };
+
+        let render_pass_descriptor = wgpu::RenderPassDescriptor {
+            label: Some("Render pass"),
+            color_attachments: &[Some(color_attachment)],
+            depth_stencil_attachment: None,
+            occlusion_query_set: None,
+            timestamp_writes: None,
+        };
+
+        command_encoder.begin_render_pass(&render_pass_descriptor);
+        self.queue.submit(std::iter::once(command_encoder.finish()));
+
+        drawable.present();
+
+        Ok(())
+    }
+
+    fn resize(&mut self, new_size: (i32, i32)) {
+        if new_size.0 > 0 && new_size.1 > 0 {
+            self.size = new_size;
+            self.config.width = new_size.0 as u32;
+            self.config.height = new_size.1 as u32;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn update_surface(&mut self) {
+        let target = unsafe {
+            wgpu::SurfaceTargetUnsafe::from_window(&self.window)
+        }.unwrap();
+
+        self.surface = unsafe {
+            self.instance.create_surface_unsafe(target)
+        }.unwrap();
+    }
+}
+
+async fn run() {
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+
+    let (mut window, events) = glfw.create_window(
+        800, 600, "It is a WGPU example", glfw::WindowMode::Windowed).unwrap();
+
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+    window.set_pos_polling(true);
+    window.make_current();
+
+    let mut state = State::new(&mut window).await;
+
+    while !state.window.should_close() {
+        glfw.poll_events();
+
+        for (_, event) in glfw::flush_messages(&events) {
+            match event {
+                glfw::WindowEvent::Key(glfw::Key::Escape, _, glfw::Action::Press, _) => {
+                    state.window.set_should_close(true)
+                }
+
+                glfw::WindowEvent::FramebufferSize(width, height) => {
+                    state.update_surface();
+                    state.resize((width, height));
+                }
+
+                glfw::WindowEvent::Pos(..) => {
+                    state.update_surface();
+                    state.resize(state.size);
+                }
+
+                e => println!("{:?}", e),
+            }
+        }
+
+        match state.render() {
+            Ok(_) => {},
+            Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                state.update_surface();
+                state.resize(state.size);
+            },
+            Err(e) => eprintln!("{:?}", e),
+        }
+
+        state.window.swap_buffers();
+    }
+}
+
+fn main() {
+    pollster::block_on(run());
+}
